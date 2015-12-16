56f8f0369abe7ecd8be3e6b49ff5effbd563e757
Miguel Provencio <numeralblue@gmail.com>
2015-12-16 08:13:05 -0700

    update example

--- examples/weather.js
+++ examples/weather.js
@@ -5,17 +5,12 @@ let options = {
   hostname: "api.openweathermap.org",
   port: 80,
   method: "GET",
-  path: "/data/2.5/weather?q=albuquerque,us&units=imperial&appid=97deb1a62310b51f6a07885321377d5d"
+  path: "/data/2.5/weather?q=london,uk&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0"
 };
 
 $http.request(options).then(function (res) {
   return res.getData().then(function (body) {
-    if (res.statusCode != 200) {
-      console.log(body);
-      return;
-    }
-
     let obj = JSON.parse(body);
-    console.log("temp", obj.main.temp);
+    console.log("The temperature in London is " + obj.main.temp + " °F");
   });
 });
